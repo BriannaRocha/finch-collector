27b7911bb5c7806e79b7ac6c684072ed6f12f5bf
BriannaRocha <rochabrianna17@gmail.com>
2024-03-04 01:18:20 -0500

    stub up tot model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tot(models.Model):
+  name = models.CharField(max_length=100)
+  gender = models.CharField(max_length=100)
+  age = models.IntegerField()
+  description = models.TextField(max_length=300)
+
+def __str__(self):
+    return self.name 
