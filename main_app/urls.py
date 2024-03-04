e323dc3ef5dea7adcda9c6453f24227c781ece1c
BriannaRocha <rochabrianna17@gmail.com>
2024-03-04 01:58:55 -0500

    add tot index and details

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('tots/', views.tot_index, name='tot-index')
+  path('tots/', views.tot_index, name='tot-index'),
+  path('tots/<int:tot_id>/', views.tot_detail, name='tot-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,7 @@ def about(request):
 def tot_index(request):
   tots= Tot.objects.all()
   return render(request, 'tots/index.html', { 'tots': tots })
+
+def tot_detail(request, tot_id):
+  tot = Tot.objects.get(id=tot_id)
+  return render(request, 'tots/detail.html', { 'tot': tot })
