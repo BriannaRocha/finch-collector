dea05c524af80e5727d4ad48820b4de629d6baa8
BriannaRocha <rochabrianna17@gmail.com>
2024-03-04 19:18:55 -0500

    add Toy to models.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tot, Feeding
+from .models import Tot, Feeding, Toy
 
 admin.site.register(Tot)
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/models.py
+++ main_app/models.py
@@ -11,10 +11,21 @@ MEALS = (
 
 # Create your models here.
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse("toy-detail", kwargs={"pk": self.id})
+
 class Tot(models.Model):
   name = models.CharField(max_length=100)
   gender = models.CharField(max_length=100)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
   description = models.TextField(max_length=300)
 
   def __str__(self):
