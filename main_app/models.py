2411730c18084ceb270b657a22e643b34a9a5bbf
BriannaRocha <rochabrianna17@gmail.com>
2024-03-04 20:49:57 -0500

    add Toys to detail.html

--- main_app/models.py
+++ main_app/models.py
@@ -25,8 +25,8 @@ class Tot(models.Model):
   name = models.CharField(max_length=100)
   gender = models.CharField(max_length=100)
   age = models.IntegerField()
-  toys = models.ManyToManyField(Toy)
   description = models.TextField(max_length=300)
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
