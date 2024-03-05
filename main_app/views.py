b140da38ccd0868f3f8c21330af3ec67c03451b9
BriannaRocha <rochabrianna17@gmail.com>
2024-03-04 19:50:09 -0500

    add Toy delete, update, and Cat Collector CSS base

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Tot
+from django.views.generic import ListView, DetailView
+from .models import Tot, Toy
 from .forms import FeedingForm
 
 def home(request):
@@ -42,4 +43,26 @@ def add_feeding(request, tot_id):
     new_feeding = form.save(commit=False)
     new_feeding.tot_id = tot_id
     new_feeding.save()
+  return redirect('tot-detail', tot_id=tot_id)
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, cat_id, toy_id):
+  Tot.objects.get(id=tot_id).toys.add(toy_id)
   return redirect('tot-detail', tot_id=tot_id)
