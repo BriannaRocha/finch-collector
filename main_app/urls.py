c6bbd575e3939d4fabbcffeca809dc83f4292729
BriannaRocha <rochabrianna17@gmail.com>
2024-03-04 19:30:13 -0500

    add Toy paths to urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
   path('tots/<int:pk>/update/', views.TotUpdate.as_view(), name='tot-update'),
   path('tots/<int:pk>/delete/', views.TotDelete.as_view(), name='tot-delete'),
   path('tots/<int:tot_id>/add-feeding/', views.add_feeding, name='add-feeding'),
+  path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
+  path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
+  path('toys/', views.ToyList.as_view(), name='toy-index'),
+  path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
+  path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
+  path('tots/<int:tot_id>/assoc-toy/<int:toy_id>/', views.assoc_toy, name='assoc-toy'),
 ]
